3ef9737192841fe240052ee20c1f277a054e15f6
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-02 22:28:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,36 @@
 require_relative '../../config/environment'
-#require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get '/posts/new' do #loads new form
+  get '/posts/new' do
     erb :new
   end
 
-  post '/posts' do  #creates a post
+  post '/posts' do 
     @post = Post.create(params)
     redirect to '/posts'
   end
 
-  get '/posts' do #loads index page
+  get '/posts' do 
     @posts = Post.all
     erb :index
   end
 
-  get '/posts/:id' do  #loads show page
+  get '/posts/:id' do 
     @post = Post.find_by_id(params[:id])
     erb :show
   end
 
-  get '/posts/:id/edit' do #loads edit form
+  get '/posts/:id/edit' do 
     @post = Post.find_by_id(params[:id])
     erb :edit
   end
 
-  post '/posts/:id' do  #updates a post
+  post '/posts/:id' do 
     @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
@@ -41,11 +39,9 @@ class ApplicationController < Sinatra::Base
   end
 
 
-  post '/posts/:id/delete' do #delete action
+  post '/posts/:id/delete' do 
     @post = Post.find_by_id(params[:id])
     @post.delete
     erb :delete
   end
-
-
 end
