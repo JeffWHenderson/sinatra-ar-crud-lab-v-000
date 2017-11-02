ff04eab4bbae65bef5643fcb61b861a9b1b6c3b0
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-02 22:22:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+#require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,31 +8,44 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-    "go to /posts/new"
+  get '/posts/new' do #loads new form
+    erb :new
   end
 
-  # get "/posts/:id" do
-  #   "200"
-  #
-  #   # erb :'show'
-  # end
-
-  get '/posts/new' do
-    erb :'new'
+  post '/posts' do  #creates a post
+    @post = Post.create(params)
+    redirect to '/posts'
   end
 
-  post '/posts' do
-    #@data = params
-    @post = Post.create(params)   #I can just load params(name: params[:post_name], content: params[:post_content])
-    @post.save
-    #"#{post.name} \n #{post.content}"
-    redirect  :'/posts'
-  end
-
-  get '/posts' do
+  get '/posts' do #loads index page
     @posts = Post.all
-    erb :'index'
+    erb :index
   end
+
+  get '/posts/:id' do  #loads show page
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do #loads edit form
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  post '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+
+  post '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
+  end
+
 
 end
