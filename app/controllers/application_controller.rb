a73da25b6f24f8444b5493092dcf1cc1a4004577
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-02 21:40:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
 
   post '/posts' do
     #@data = params
-    post = Post.create(name: params[:post_name], content: params[:post_content])
+    post = Post.create(params)   #I can just load params(name: params[:post_name], content: params[:post_content])
     post.save
     "#{post.name} \n #{post.content}"
   end
